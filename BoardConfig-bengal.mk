b9701ff2897eba8f279866dc27e18af8db55b886
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-20 03:26:29 +0200

    bengal: Declare BOARD_VENDOR
    * Needed for xiaomi-perfmgr power AIDL guards
    
    Change-Id: Idbb48e0f9b01090a456ad5d3f2d51f4dfab55672
    Signed-off-by: darknius09 <darkniuslightness@gmail.com>
    Signed-off-by: darknius20 <darknius21@gmail.com>

--- BoardConfig-bengal.mk
+++ BoardConfig-bengal.mk
@@ -148,6 +148,7 @@ TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
+BOARD_VENDOR := xiaomi
 
 # Recovery
 #BOARD_INCLUDE_DTB_IN_BOOTIMG := true
