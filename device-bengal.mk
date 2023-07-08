d19712df180852269a023b29176aa3f72c1f840d
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-07-08 12:42:17 +0700

    bengal: Drop AIDL power extension
    Change-Id: I8719fbff9bf81b390a63bf4404de11238327aed8
    Signed-off-by: darknius09 <darkniuslightness@gmail.com>
    Signed-off-by: darknius20 <darknius21@gmail.com>

--- device-bengal.mk
+++ device-bengal.mk
@@ -314,12 +314,6 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
