06b738f91863ca6e7db94b8676b8d89e1b626150
darknius09 <darkniuslightness@gmail.com>
2023-12-15 08:56:04 +0000

    Revert "bengal: remove odm related stuff"
    This reverts commit 8e2b5bf2f4dd570583757c38a04c5c2458aa1b28.

--- BoardConfig-bengal.mk
+++ BoardConfig-bengal.mk
@@ -143,6 +143,7 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_ODM := odm
 
 # Properties
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
